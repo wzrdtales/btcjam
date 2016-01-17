04b135c141993a911e892032187be9a7960b4b0d
Tobias Gurtzick <magic@wizardtales.com>
2016-01-17 14:23:04 +0100

    add migraiton

---
+++ migrations/20160117131445-addVirtualColumns.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+
+  return db.runSql( 'ALTER TABLE `listing` \
+    ADD COLUMN `deleted` BIT(1) AS \
+    (COLUMN_GET(data, \'deleted_listing\' AS unsigned integer)) \
+    PERSISTENT AFTER `deleted`;' )
+  .then( function() {
+
+    return db.runSql( 'ALTER TABLE `listing` \
+      ADD COLUMN `status` tinyint(3) unsigned AS \
+      (COLUMN_GET(data, \'listing_status_id\' AS unsigned integer)) \
+      PERSISTENT AFTER `deleted`;' );
+  } )
+  .then( function() {
+
+    return db.runSql( 'ALTER TABLE `listing` \
+      ADD COLUMN `expire_date` datetime AS \
+      (DATE_ADD(COLUMN_GET(data, \'dtclose\' as datetime), \
+        INTERVAL COLUMN_GET(data, \'term_days\' as int) DAY)) \
+      PERSISTENT AFTER `deleted`;' );
+  } );
+};
+
+exports.down = function(db) {
+
+  return db.removeColumn( 'listing', 'deleted' )
+  .then( function() {
+
+    return db.removeColumn( 'listing', 'status' );
+  } )
+  .then( function() {
+
+    return db.removeColumn( 'listing', 'expire_date' );
+  } );
+};
